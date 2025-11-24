59be8a6da6cfab610d69eb387c3c35619cfe68ca
Niranjan <niranjanwork1311@gmail.com>
2025-11-24 12:55:24 +0530

    added print statement

--- tuples.py
+++ tuples.py
@@ -1,26 +1,26 @@
-# # Tuple packing and unpacking
-# a = "niranjan"
-# b = 21
-# c = "doctor"
-# tuple_pack = a, b, c
-# print(tuple_pack)
-# name, age, profession = tuple_pack
-# print(name)
-# print(age)
-# print(profession)
+# Tuple packing and unpacking
+a = "niranjan"
+b = 21
+c = "doctor"
+tuple_pack = a, b, c
+print(tuple_pack)
+name, age, profession = tuple_pack
+print(name)
+print(age)
+print(profession)
 
-# # trying to modify elements in a tuple
-# a = (1, 2, 3, 4, 5)
-# list_elements = list(a)
-# print(list_elements)
-# list_elements.append(6)
-# print(list_elements)
-# tuple1 = tuple(list_elements)
-# print(tuple1)
+# trying to modify elements in a tuple
+a = (1, 2, 3, 4, 5)
+list_elements = list(a)
+print(list_elements)
+list_elements.append(6)
+print(list_elements)
+tuple1 = tuple(list_elements)
+print(tuple1)
 
 # tuple functions
 a = (1, 2, 3, 4)
-# print(len(a))
+print(len(a))
 
 sorted_data = tuple(sorted(a))
 print(sorted_data)
@@ -30,3 +30,4 @@ print(sum(a))
 print(min(a))
 
 print(max(a))
+print("hello world")
